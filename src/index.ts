cddf78e493b3ed9736de2e37e4d847bc63207427
Personal Use <awaishk640@gmail.com>
2023-12-30 23:53:33 +0530

    Added port number

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 import app from "./server"
-const port = parseInt(process.env.PORT as string) || 4000;
+const port = 4000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
